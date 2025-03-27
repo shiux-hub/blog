51f3714056f719b0ed4fd6b4a6619210fcb5aff3
root <root@MS-RRPAEGLDTKHN.h3c.huawei-3com.com>
2025-03-27 16:46:44 +0800

    switch to vitepress

---
+++ eslint.config.js
@@ -0,0 +1,6 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  ignores: ['node_modules', '**/node_modules/**', 'dist', '**/dist/**', '.vitepress/dist', '.vitepress/dist/**', '.cache', '**/.cache/**', 'auto-imports.d.ts', '**/auto-imports.d.ts/**', 'components.d.ts', '**/components.d.ts/**'],
+  vue: true,
+})
--- source/archive.md
+++
@@ -1,6 +0,0 @@
----
-title: 文章归档
-date: 2019-09-09 06:37:37
-layout: archives
-comments: false
----
